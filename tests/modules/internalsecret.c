467c06b64c8523ef3d4b6b06a508bdfa7943deb6
raz-mon <razmonsonego2@gmail.com>
2025-02-04 10:48:42 +0200

    Revert module change

--- tests/modules/internalsecret.c
+++ tests/modules/internalsecret.c
@@ -60,8 +60,7 @@ int call_rm_call(RedisModuleCtx *ctx, RedisModuleString **argv, int argc, RMCall
             rep = RedisModule_Call(detached_ctx, cmd, "vCE", argv + 2, argc - 2);
             break;
         case RM_CALL_REPLICATED:
-            // Replicated call (verbatim), do not use the current client
-            rep = RedisModule_Call(ctx, cmd, "vE!", argv + 2, argc - 2);
+            rep = RedisModule_Call(ctx, cmd, "vE", argv + 2, argc - 2);
     }
 
     if(!rep) {
@@ -81,6 +80,8 @@ int call_rm_call(RedisModuleCtx *ctx, RedisModuleString **argv, int argc, RMCall
     } else {
         RedisModule_ReplyWithCallReply(ctx, rep);
         RedisModule_FreeCallReply(rep);
+        if (mode == RM_CALL_REPLICATED)
+            RedisModule_ReplicateVerbatim(ctx);
     }
 
     if (mode == RM_CALL_WITHDETACHEDCLIENT) {
