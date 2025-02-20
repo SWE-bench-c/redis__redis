3d05ecced5101d2de3f5750fc9eb8d87a6d67572
debing.sun <debing.sun@redis.com>
2025-02-20 20:55:05 +0800

    Revert changes not related to this PR

--- tests/unit/moduleapi/datatype.tcl
+++ tests/unit/moduleapi/datatype.tcl
@@ -193,7 +193,6 @@ start_server {tags {"modules"}} {
                 set end_time [expr {[clock seconds] + 10}]
                 set speed_restored 0
                 while {[clock seconds] < $end_time} {
-                    for {set i 0} {$i < 500} {incr i} {
                     switch [expr {int(rand() * 3)}] {
                         0 {
                             # Randomly delete a key
@@ -211,19 +210,18 @@ start_server {tags {"modules"}} {
                         }
                         2 {
                             # Randomly generate a new key
-                            set random_key "key_[expr {int(rand() * 1000000)}]"
+                            set random_key "key_[expr {int(rand() * 10000)}]"
                             r datatype.set $random_key 1 $dummy
                         }
-                    } ;# end of switch
-                    } ;# end of for
+                    }
 
-                    if {{[count_log_message $loglines "*Starting active defrag, frag=*%, frag_bytes=*, cpu=5?%*"]} > 1} {
+                    # Wait for defragmentation speed to restore.
+                    if {[s active_defrag_running] > 25} {
                         set speed_restored 1
                         break;
                     }
                 }
 
-                # Make sure the speed is restored
                 assert_equal $speed_restored 1
 
                 # After the traffic disappears, the defragmentation speed will decrease again.
